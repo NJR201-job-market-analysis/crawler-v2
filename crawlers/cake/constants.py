57ba5a9f50b7f3c92bb53fbec460670393a9dad4
falloutnicole <falloutnicole@gmail.com>
2025-08-04 17:38:16 +0800

    feat: update category

--- crawlers/cake/constants.py
+++ crawlers/cake/constants.py
@@ -1,120 +1,33 @@
 job_categories = [
-    {
-        "id": "back-end-engineer",
-        "name": "後端工程師",
-    },
-    {
-        "id": "front-end-engineer",
-        "name": "前端工程師",
-    },
-    {
-        "id": "software-engineer",
-        "name": "軟體工程師",
-    },
-    {
-        "id": "full-stack-development",
-        "name": "全端工程師",
-    },
-    {
-        "id": "blockchain-platform-engineer",
-        "name": "區塊鏈軟體工程師",
-    },
-    {
-        "id": "qa-test-engineer",
-        "name": "QA 測試工程師",
-    },
-    {
-        "id": "devops-system-admin",
-        "name": "DevOps 系統管理員",
-    },
-    {
-        "id": "data-engineer",
-        "name": "數據工程師",
-    },
-    {
-        "id": "python-developer",
-        "name": "Python 開發人員",
-    },
-    {
-        "id": "data-scientist",
-        "name": "資料科學家",
-    },
-    {
-        "id": "ios-developer",
-        "name": "iOS 開發人員",
-    },
-    {
-        "id": "java-developer",
-        "name": "Java 開發人員",
-    },
-    {
-        "id": "research-r-d",
-        "name": "研發",
-    },
-    {
-        "id": "web-developer",
-        "name": "網站開發人員",
-    },
-    {
-        "id": "android-developer",
-        "name": "Android 開發人員",
-    },
-    {
-        "id": "machine-learning-engineer",
-        "name": "機器學習工程師",
-    },
-    {
-        "id": "php-developer",
-        "name": "PHP 開發人員",
-    },
-    {
-        "id": "ruby-on-rails-developer",
-        "name": "Ruby on Rails 開發人員",
-    },
-    {
-        "id": "system-architecture",
-        "name": "系統架構",
-    },
-    {
-        "id": "c-c-developer",
-        "name": "C/C++ 開發人員",
-    },
-    {
-        "id": "big-data-engineer",
-        "name": "大數據工程師",
-    },
-    {
-        "id": "node-js-developer",
-        "name": "Node.js 開發人員",
-    },
-    {
-        "id": "system-analyst",
-        "name": "系統分析師",
-    },
-    {
-        "id": "development-manager",
-        "name": "開發經理",
-    },
-    {
-        "id": "database",
-        "name": "資料庫管理員",
-    },
-    {
-        "id": "chief-technology-officer",
-        "name": "技術長",
-    },
-    {
-        "id": "app-developer",
-        "name": "跨平台應用程式開發人員",
-    },
-    {
-        "id": "net-developer",
-        "name": ".NET 開發人員",
-    },
-    {
-        "id": "technical-manager",
-        "name": "技術經理",
-    },
+    {"id": "back-end-engineer", "name": "後端工程師"},
+    {"id": "front-end-engineer", "name": "前端工程師"},
+    {"id": "software-engineer", "name": "軟體工程師"},
+    {"id": "full-stack-development", "name": "全端工程師"},
+    {"id": "blockchain-platform-engineer", "name": "區塊鏈工程師"},
+    {"id": "qa-test-engineer", "name": "軟體測試工程師"},
+    {"id": "devops-system-admin", "name": "DevOps工程師"},
+    {"id": "data-engineer", "name": "資料工程師"},
+    {"id": "python-developer", "name": "軟體工程師"},
+    {"id": "data-scientist", "name": "資料科學家"},
+    {"id": "ios-developer", "name": "APP工程師"},
+    {"id": "java-developer", "name": "軟體工程師"},
+    {"id": "research-r-d", "name": "研發工程師"},
+    {"id": "web-developer", "name": "網站開發人員"},
+    {"id": "android-developer", "name": "APP工程師"},
+    {"id": "machine-learning-engineer", "name": "機器學習工程師"},
+    {"id": "php-developer", "name": "軟體工程師"},
+    {"id": "ruby-on-rails-developer", "name": "軟體工程師"},
+    {"id": "system-architecture", "name": "系統工程師"},
+    {"id": "c-c-developer", "name": "軟體工程師"},
+    {"id": "big-data-engineer", "name": "資料工程師"},
+    {"id": "node-js-developer", "name": "軟體工程師"},
+    {"id": "system-analyst", "name": "系統分析師"},
+    {"id": "development-manager", "name": "專案/技術主管"},
+    {"id": "database", "name": "資料庫管理人員"},
+    {"id": "chief-technology-officer", "name": "專案/技術主管"},
+    {"id": "app-developer", "name": "APP工程師"},
+    {"id": "net-developer", "name": "軟體工程師"},
+    {"id": "technical-manager", "name": "專案/技術主管"},
 ]
 
 job_type_dict = {
